168ece7588b485c803d9a25e9a47cf04862d0c28
Justin Fiedler <jstxgu@gmail.com>
2025-05-17 23:57:18 +0200

    chore: initial project setup for frontend (Angular) and backend (Spring Boot)

---
+++ backend/src/test/java/de/teamthree/learn_hub/LearnHubApplicationTests.java
@@ -0,0 +1,13 @@
+package de.teamthree.learn_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
